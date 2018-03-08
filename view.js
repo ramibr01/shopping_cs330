504e02a1d4feae0b1de4ab5feee5e7dd6becbf4b
ramibr01 <ramibr01@luther.edu>
2018-03-08 09:20:41 -0600

    Add files via upload

---
+++ view.js
@@ -0,0 +1,30 @@
+class ShoppingView {
+  constructor(model) {
+    //
+    model.subscribe(this.redrawList.bind(this))
+  }
+
+  redrawList(shoppingList, msg) {
+    let tbl = document.getElementById("shoppinglist")
+    tbl.innerHTML = ""
+    for (let item of shoppingList.newItems) {
+      this.addRow(item, tbl)
+    }
+  }
+
+  addRow(item, parent) {
+    let row = document.createElement("tr")
+    row.classList.add(item.priority)
+    let cb = document.createElement("input")
+    cb.type = "checkbox"
+    cb.classList.add("form-control")
+    row.appendChild(cb)
+
+    for (let val of ['name', 'quantity', 'store', 'section',  'price']) {
+      let td = document.createElement("td")
+      id.innerHTML = item[val]
+      row.appendChild(td)
+    }
+    parent.appendChild(row)
+  }
+}
